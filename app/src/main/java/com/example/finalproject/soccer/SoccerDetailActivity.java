6ce191febaf423b854763a6b190ff019dbea436e
Ye Z <zhan0651@algonquinlive.com>
2020-07-14 23:54:08 -0400

    Soccer v4

--- app/src/main/java/com/example/finalproject/soccer/SoccerDetailActivity.java
+++ app/src/main/java/com/example/finalproject/soccer/SoccerDetailActivity.java
@@ -8,6 +8,7 @@ import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
@@ -15,6 +16,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.ImageView;
+import android.widget.MediaController;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -41,6 +43,8 @@ public class SoccerDetailActivity extends AppCompatActivity {
 
     private ProgressBar pbDetail;
     private SQLiteDatabase db;
+    MediaController mediaController;
+    int position = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -85,9 +89,22 @@ public class SoccerDetailActivity extends AppCompatActivity {
 
         Button btnHighlight = findViewById(R.id.btnSoccerDHighlight);
         btnHighlight.setOnClickListener(v -> {
+
             //show video
-            VideoView vv = findViewById(R.id.soccerVideoView);
-            // set VideoView.VISIBLE = true
+            VideoView player = findViewById(R.id.soccerVideoView);
+            player.setVisibility(View.VISIBLE);
+
+            mediaController = new MediaController(this);
+            mediaController.setAnchorView(player);
+            mediaController.setMediaPlayer(player);
+
+            player.setMediaController(mediaController);
+            player.setVideoURI(Uri.parse(videoUrl));
+            player.setOnPreparedListener( video ->{
+                player.seekTo(position);
+                if(position == 0)
+                    player.start();
+            });
         });
 
     }
